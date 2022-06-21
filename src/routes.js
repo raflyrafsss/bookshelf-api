e3d22f1a08dd6c75c868ec545a2077c50eb10d70
Rafly Rafsanjani <38741998+raflyrafsss@users.noreply.github.com>
2022-06-21 13:40:12 +0700

    setup api routes

---
+++ src/routes.js
@@ -0,0 +1,23 @@
+import {
+  addBook,
+  getBookById,
+  getBooks,
+} from './controller/book.controller.js';
+
+export const routes = [
+  {
+    method: 'POST',
+    path: '/books',
+    handler: addBook,
+  },
+  {
+    method: 'GET',
+    path: '/books',
+    handler: getBooks,
+  },
+  {
+    method: 'GET',
+    path: '/books/{id}',
+    handler: getBookById,
+  },
+];
